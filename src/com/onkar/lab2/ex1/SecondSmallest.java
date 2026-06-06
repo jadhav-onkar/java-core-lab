66a8c0a1b83160bf1b13c9674fd00e3b3b748d60
Onkar Jadhav <onkar.rajendra.jadhav@ibm.com>
2026-06-06 11:46:43 +0530

    Lab 2

--- src/com/onkar/lab2/ex1/SecondSmallest.java
+++ src/com/onkar/lab2/ex1/SecondSmallest.java
@@ -1,34 +1,46 @@
-//package com.onkar.lab2.ex1;
-//
-//import java.util.Scanner;
-//
-//public class SecondSmallest {
-//	
-//	public static int secondHighestNumber(int arr[]) {
-//		int highest = arr[0];
-//		int secondHighest = arr[0];
-//		
-//		for (int i = 0; i < arr.length; i++) {
-//			if (arr[i] > secondHighest) {
-//				highest = arr[i];
-//			}
-//		}
-//		return 0;
-//	}
-//	
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter Length of Array: ");
-//		int len = sc.nextInt();
-//		
-//		int arr[] = new int[len];
-//		
-//		for (int i = 0; i < len; i++) {
-//			System.out.print("Enter number "+ (i+1) + " : ");
-//			int n = sc.nextInt();
-//			arr[i] = n;
-//		}
-//		
-//		sc.close();
-//	}
-//}
+package com.onkar.lab2.ex1;
+
+import java.util.Scanner;
+
+public class SecondSmallest {
+	
+	public static int secondSmallestNumber(int arr[]) {
+		int smallest = Integer.MAX_VALUE;
+		int secondSmallest = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < smallest) {
+				secondSmallest = smallest;
+				smallest = arr[i];
+			}
+			
+			if (arr[i] < secondSmallest && arr[i] > smallest) {
+				secondSmallest = arr[i];
+			}
+		}
+		return secondSmallest;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Length of Array: ");
+		int len = sc.nextInt();
+		
+		while (len < 1) {
+			System.out.print("Enter Length of Array: ");
+			len = sc.nextInt();
+		}
+		
+		int arr[] = new int[len];
+		
+		for (int i = 0; i < len; i++) {
+			System.out.print("Enter number " + (i + 1) + " : ");
+			int n = sc.nextInt();
+			arr[i] = n;
+		}
+		
+		System.out.println("Second Highest Number: " + SecondSmallest.secondSmallestNumber(arr));
+		
+		sc.close();
+	}
+}
